472c28364ef179c8d935183a8285c027f4d8d61f
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-12-20 10:33:53 +0000

    Dec 20, 2023, 4:03 PM

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 function App() {
   return (
-    <div className='bg-blue-500 p-4'>
+    <div className='bg-blue-500 p-8'>
       <h1 className="text-2xl font-bold text-white">Welcome to My React App</h1>
       <p className="text-lg text-white mt-2">This is a simple example using Tailwind CSS.</p>
     </div>
