ef664cb5546793d5f928f079ec696f1cdb227d2c
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-12-20 10:42:59 +0000

    Dec 20, 2023, 4:12 PM

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
 function App() {
   return (
-    <div className='bg-blue-500 p-0'>
-      <h1 className="text-2xl font-bold text-white">Welcome to My React App</h1>
-      <p className="text-lg text-white mt-2">This is a simple example using Tailwind CSS.</p>
+    <div className="flex h-screen bg-gray-800">
+      {/* Left Sidebar */}
+      <div className="flex-shrink-0 w-16 bg-gray-900">
+        {/* Your navigation links/icons go here */}
+        <p className="text-white text-lg p-2">Logo</p>
+        <a href="#" className="text-white block p-2 hover:bg-gray-700">
+          Link 1
+        </a>
+        <a href="#" className="text-white block p-2 hover:bg-gray-700">
+          Link 2
+        </a>
+        {/* Add more links/icons as needed */}
+      </div>
+
+      {/* Main Content */}
+      <div className="flex-1 p-4">
+        {/* Your main content goes here */}
+        <h1 className="text-2xl font-bold text-white">Welcome to My App</h1>
+        <p className="text-white mt-2">This is the main content area.</p>
+      </div>
     </div>
   );
 }
